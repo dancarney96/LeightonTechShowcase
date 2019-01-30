c328731fea66588eb7d4ab0e80a3fd88e6242450
dancarney96 <35928056+dancarney96@users.noreply.github.com>
2019-01-30 22:04:55 +0000

    Added dist and sources files
    Uploaded project code and distribution files

---
+++ src/app/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getItems() {
+    const requestOptions = {
+      headers: new HttpHeaders({
+        'x-api-key': 'zQo4PPqD862IwDIQRZub8gX4dqjA3aW2DDhI6UF4',
+      }),
+    };
+
+    return this.http.get('https://27gmrimn45.execute-api.eu-west-2.amazonaws.com/demos/leighton-demo-api?TableName=products', requestOptions)
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  items: Object;
+  objectKeys = Object.keys;
+  purchase: string;
+
+  constructor(private data: DataService) { this.purchase = ""; }
+
+  ngOnInit() {
+    this.data.getItems().subscribe(data => {
+        this.items = data
+      }
+    );
+  }
+
+  inStock = function (i) {
+    if (this.items.Items[i].size.small != '0') {
+      return true;
+    }
+    if (this.items.Items[i].size.medium != '0') {
+      return true;
+    }
+    if (this.items.Items[i].size.large != '0') {
+      return true;
+    }
+    return false;
+  }
+
+  purchaseHandle = function(i) {
+    this.purchase = "You have purchased a " + this.items.Items[i].colour + " " + this.items.Items[i].name;
+  }
+}
